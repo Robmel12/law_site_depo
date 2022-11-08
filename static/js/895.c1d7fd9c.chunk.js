c6aa0ac5f8e8440852fbbd06a680225452fd1c63
Robmel12 <rob.melnyk@gmail.com>
2022-11-08 16:36:13 -0600

    Updates

--- static/js/895.c1d7fd9c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunklaw_site=self.webpackChunklaw_site||[]).push([[895],{3895:function(e,t,s){s.r(t);s(2791);var n=s(184);t.default=function(e){return(0,n.jsx)("h1",{className:"consulting",style:{width:"100%",height:"600px",color:"white"},children:"Consulting"})}}}]);
-//# sourceMappingURL=895.c1d7fd9c.chunk.js.map
