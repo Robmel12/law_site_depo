bce66dadae86697793fa1f4f9e3a7014e777ca20
Robmel12 <rob.melnyk@gmail.com>
2022-11-09 21:17:27 -0600

    removed console.log from topscroll function

--- src/components/pages/Pagefunctions/ScrollToTop.js
+++ src/components/pages/Pagefunctions/ScrollToTop.js
@@ -4,7 +4,6 @@ import { useLocation } from "react-router-dom";
 export default function Routes() {
   const location = useLocation();
   // Scroll to top if path changes
-  console.log("used scrolltotop");
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
   }, [location]);
