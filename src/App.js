85dbfdbe223d92ae5239b9e5124ded73f0fd04d3
Robmel12 <rob.melnyk@gmail.com>
2022-11-09 18:35:45 -0600

    changed to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, lazy, Suspense } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 // import Home from './components/pages/Home';
 import { Home } from "./components/pages/Home";
