2f1cceb7cd22e284c00c0d576d669dac7f5a5e56
Robmel12 <rob.melnyk@gmail.com>
2022-12-08 06:43:59 -0600

    fixed jumbotron form

--- src/components/sections/Jumbotron/Form.js
+++ src/components/sections/Jumbotron/Form.js
@@ -2,36 +2,41 @@ import React, { useState } from "react";
 import { FaHeadset, FaTimes } from "react-icons/fa";
 export default function Form(props) {
   const [email, setEmail] = useState("");
-  const [validEmail, setValidEmail] = useState(true)
+  const [validEmail, setValidEmail] = useState(true);
   const [message, setMessage] = useState("");
-  const [showForm, setShowForm] = useState(true)
+  const [showForm, setShowForm] = useState(true);
 
-  const Valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-  
+  const Valid = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+
   const styles = props.styles;
-  const handleChange = (e)=>{
+  const handleChange = (e) => {
     setEmail(e.target.value);
-    setValidEmail(Valid.test(email));
-  }
-  const handleClick = (e) =>{
+  };
+  const handleClick = (e) => {
     e.preventDefault();
-    if(validEmail) {setShowForm(false)
+    if (Valid.test(email)) {
+      setValidEmail(Valid.test(email));
+      setShowForm(false);
       const EmailMessage = {
         email: email,
         message: message,
       };
-      console.log(EmailMessage);}
-  }
+      console.log(EmailMessage);
+    } else {
+      setValidEmail(Valid.test(email));
+    }
+  };
 
-  const handleNewMessageClick = (e)=>{
+  const handleNewMessageClick = (e) => {
     e.preventDefault();
-   setShowForm(true);
-  }
-
+    setShowForm(true);
+  };
 
   return (
     <div
-      className={`${styles.container} email-form-container ${!showForm && styles.noShow}` }
+      className={`${styles.container} email-form-container ${
+        !showForm && styles.noShow
+      }`}
     >
       <h3>
         <FaHeadset
@@ -45,29 +50,44 @@ export default function Form(props) {
           onClick={props.onClick}
         />
       )}
-      {!validEmail && <p style={{color: 'red'}}>Email is invalid</p>}
-      {showForm ? <form className="email-form" >
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="Your E-mail here.."
-          onChange={handleChange}
-        />
-        <textarea
-          className={styles.textarea}
-          placeholder="Send us a message!"
-          cols="30"
-          rows="9"
-          onChange={(e) => setMessage(e.target.value)}
-        ></textarea>
-        <button onClick={handleClick} className={`${styles.button} form-btn`}>
-          Message Us
-        </button>
-      </form>: <><p className={styles.confirm}>We will send you and email shortly at {email}</p>
-      <div className={styles.confirmButtonContainer}>
-      <button className={styles.rewMessageButton}onClick={handleNewMessageClick}>New Message</button>
-      <button className={styles.resendButton}>Resend Email</button></div>
-      </>}
+      {!validEmail && <p style={{ color: "red" }}>Email is invalid</p>}
+      {showForm ? (
+        <form className="email-form">
+          <input
+            className={styles.input}
+            type="email"
+            placeholder="Your E-mail here.."
+            onChange={handleChange}
+            value={email}
+          />
+          <textarea
+            className={styles.textarea}
+            placeholder="Send us a message!"
+            cols="30"
+            rows="9"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+          <button onClick={handleClick} className={`${styles.button} form-btn`}>
+            Message Us
+          </button>
+        </form>
+      ) : (
+        <>
+          <p className={styles.confirm}>
+            We will send you and email shortly at {email}
+          </p>
+          <div className={styles.confirmButtonContainer}>
+            <button
+              className={styles.rewMessageButton}
+              onClick={handleNewMessageClick}
+            >
+              New Message
+            </button>
+            <button className={styles.resendButton}>Resend Email</button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
